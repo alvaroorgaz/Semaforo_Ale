52949ab9150c681f44aa4a811a80e5ceec37d34c
alvaroorgaz <alvaro.orgazmonleon24@campusfp.es>
2026-05-07 13:38:04 +0200

    Convertir dashboard en app instalable

--- script.js
+++ script.js
@@ -987,3 +987,10 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
   }
 });
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js");
+  });
+}
